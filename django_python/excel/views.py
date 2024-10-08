fc113b86638a59421d21f4859c8e1b7dc3951fea
franciscoj <frankmazta@gmail.com>
2024-10-08 12:07:18 -0400

    se agrego el link del estilo

--- django_python/excel/views.py
+++ django_python/excel/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index1(request):
+def index(request):
     return render(request, "excel/index.html")
 
-def index0(request):
-    return HttpResponse("Hello, world!")
